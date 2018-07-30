aab093d5514d4eb45f1a5aa21fedfe804f0cb3bc
Milo Wissig <mwissig@gmail.com>
2018-07-30 19:44:58 -0400

    earth disappears

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
+var adRan = false;
+
+function waitFiveSeconds() {
+  setTimeout(function() {
+    document.getElementById('earthbox').classList.add("hide");
+  }, 5000);
+}
+
 function launchMe() {
   var response = prompt("Are you sure? You will not be able to return to Earth.\nPlease type Y or N");
   if (response == "Y" || response == "y" || response == "yes") {
     document.getElementById('earthbox').classList.add("flyaway");
-        document.getElementById('earthbox').classList.remove("spin");
-        document.getElementById('launchme').classList.add("rocketshadow");
-                document.getElementById('letsgo').classList.add("rocketshadow");
-  }
-  else if (response == "N" || response == "n" || response == "no") {
+    document.getElementById('earthbox').classList.remove("spin");
+    document.getElementById('launchme').classList.add("rocketshadow");
+    document.getElementById('letsgo').classList.add("rocketshadow");
+    waitFiveSeconds();
+  } else if (response == "N" || response == "n" || response == "no") {
     alert("Take your time, this is a major decision.");
-  }
-  else {
+  } else {
     alert("Invalid response");
   }
 }
+
 function leftScreen() {
-  alert("Don\'t leave yet!\nIf you refer a friend or family member you can get 50% off!")
+  if (adRan == false) {
+    alert("Don\'t leave yet!\nIf you refer a friend or family member you can get 50% off!")
+    adRan = true;
+  }
 }
-console.log('working');
